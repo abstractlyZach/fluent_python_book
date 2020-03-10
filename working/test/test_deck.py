9869d98960fe446dab814ad7f76b6e093abc9ff0
abstractlyZach <zach3lee@gmail.com>
2020-03-09 17:11:19 -0700

    Add card rankings

--- working/test/test_deck.py
+++ working/test/test_deck.py
@@ -1,3 +1,5 @@
+import random
+
 import deck
 
 
@@ -32,3 +34,25 @@ def test_deck_check_for_in_operator(unshuffled_deck):
 def test_deck_card_not_in_deck(unshuffled_deck):
     card = deck.Card(suit="spades", rank="beasts")
     assert card not in unshuffled_deck
+
+
+def test_sorting_deck_low_cards(unshuffled_deck):
+    low_cards = [
+        deck.Card("2", suit) for suit in "clubs diamonds hearts spades".split()
+    ] + [deck.Card("3", "clubs")]
+    sorted_deck = sorted(unshuffled_deck, key=deck.spades_high)
+    for expected_card, actual_card in zip(low_cards, sorted_deck):
+        assert expected_card == actual_card
+
+
+def test_sorting_deck_high_cards(unshuffled_deck):
+    high_cards = [deck.Card("K", "spades")] + [
+        deck.Card("A", suit) for suit in "clubs diamonds hearts spades".split()
+    ]
+    sorted_deck = sorted(unshuffled_deck, key=deck.spades_high)
+    for expected_card, actual_card in zip(high_cards, sorted_deck[-len(high_cards) :]):
+        assert expected_card == actual_card
+
+
+def test_random_card_draw_doesnt_throw_error(unshuffled_deck):
+    random.choice(unshuffled_deck)
